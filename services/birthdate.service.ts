f69b757de2e8ee580d61abaf975e5da22b4dab2a
Quentin MOLERO <quentin.molero@estiam.com>
2021-04-28 21:47:13 +0200

    Add birthdate service and tests

---
+++ services/birthdate.service.ts
@@ -0,0 +1,6 @@
+export class BirthdateService {
+
+    isBirthdateValid(birthdate: Date): boolean {
+        throw new Error("Not implemented yet");
+    }
+}
---
+++ tests/models/birthdate.test.ts
@@ -0,0 +1,32 @@
+import {BirthdateService} from '../../services/birthdate.service';
+
+describe("Tests to validate the email service", () => {
+
+    let birthdateService: BirthdateService;
+    let test_birthdate: Date;
+
+    beforeEach(() => {
+        birthdateService = new BirthdateService();
+        test_birthdate = new Date();
+    });
+
+    it("Should return true when called with a valid birthdate", () => {
+        test_birthdate = new Date("2000-01-01")
+        expect(birthdateService.isBirthdateValid(test_birthdate)).toBeTruthy();
+    });
+
+    it("Should return false when called with an invalid birthdate", () => {
+        expect(birthdateService.isBirthdateValid(new Date())).toBeFalsy();
+    });
+
+    it("Should return true when called with a barely legal birthdate", () => {
+        test_birthdate.setUTCFullYear(test_birthdate.getFullYear() - 13);
+        expect(birthdateService.isBirthdateValid(test_birthdate)).toBeTruthy();
+    });
+
+    it("Should return false when called with a barely illegal birthdate", () => {
+        test_birthdate.setUTCFullYear(test_birthdate.getFullYear() - 13);
+        test_birthdate.setDate(test_birthdate.getDay() + 1);
+        expect(birthdateService.isBirthdateValid(test_birthdate)).toBeFalsy();
+    });
+})
