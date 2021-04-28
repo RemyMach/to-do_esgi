106f074f694932d50e438486cc99b336a2a2c416
Quentin MOLERO <quentin.molero@estiam.com>
2021-04-28 20:38:23 +0200

    Add password to User model

--- models/user.model.ts
+++ models/user.model.ts
@@ -3,7 +3,7 @@ interface UserProps {
     firstName: string;
     lastName: string;
     birth_date: Date;
-
+    password: string;
 }
 
 export class UserModel implements UserProps{
@@ -11,21 +11,23 @@ export class UserModel implements UserProps{
     email: string;
     firstName: string;
     lastName: string;
+    password: string;
 
-    constructor(birth_date: Date, email: string, name: string, surname: string) {
+    constructor(birth_date: Date, email: string, firstname: string, lastname: string, password: string) {
         this.birth_date = birth_date;
         this.email = email;
-        this.firstName = name;
-        this.lastName = surname;
+        this.firstName = firstname;
+        this.lastName = lastname;
+        this.password = password;
+    }
+
+    validatePropsAreNotEmpty(): boolean {
+        return this.firstName !== "" && this.lastName !== "" && this.email !== "" && this.password !== "";
     }
 
     validateEmail(): boolean {
         const re = /\S+@\S+\.\S+/;
         return this.email.match(re) !== null;
-    }
-
-    validatePropsAreNotEmpty(): boolean {
-        return this.firstName !== "" && this.lastName !== "" && this.email !== "";
     }
 
     validateBirthDateIsLessRecentThan13Years(): boolean {
@@ -34,8 +36,12 @@ export class UserModel implements UserProps{
         return this.birth_date < date_before_13_years;
     }
 
+    validatePasswordLenght(): boolean {
+        return this.password.length >= 8 && this.password.length <= 40;
+    }
+
     isValid(): boolean {
 
-        return this.validatePropsAreNotEmpty() && this.validateEmail() && this.validateBirthDateIsLessRecentThan13Years();
+        return this.validatePropsAreNotEmpty() && this.validateEmail() && this.validateBirthDateIsLessRecentThan13Years() && this.validatePasswordLenght();
     }
 }
