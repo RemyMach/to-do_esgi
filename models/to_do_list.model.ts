ffaffc23ff782ea8e327246c79e66e7a5803613f
QuentinR <rouvilleq@gmail.com>
2021-04-29 20:16:58 +0200

    Add "addNewItem" method to to_do_list.model.ts

--- models/to_do_list.model.ts
+++ models/to_do_list.model.ts
@@ -11,4 +11,37 @@ export class ToDoListModel implements ToDoListProps
     constructor() {
         this.items = [];
     }
+
+    public async addNewItem(item: Item): Promise<boolean>
+    {
+        if(this.items.length >= 10 || !await this.checkLastTimeInsertTime()) {
+            return false;
+        }
+
+        this.items.push(item);
+
+        if(this.items.length === 8) {
+            await this.sendMail();
+        }
+
+        return true;
+    }
+
+    private async getLastItemDateTime(): Promise<Date>
+    {
+        return this.items[this.items.length - 1].createdAt;
+    }
+
+    private async checkLastTimeInsertTime(): Promise<boolean>
+    {
+        const currentDateTime = new Date();
+        currentDateTime.setMinutes(currentDateTime.getMinutes() - 30);
+        console.log(currentDateTime);
+        return await this.getLastItemDateTime() < currentDateTime;
+    }
+
+    private async sendMail(): Promise<void>
+    {
+        // Need to be implemented
+    }
 }
