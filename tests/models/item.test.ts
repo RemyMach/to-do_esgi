5a0f709c10a75a25cd90fef0d78285feda65d131
RemyMach <remygt@hotmail.fr>
2021-04-29 22:02:41 +0200

    add mock test for item creation

--- tests/models/item.test.ts
+++ tests/models/item.test.ts
@@ -2,31 +2,66 @@ import {ItemModel} from '../../models/item.model';
 import {ItemValidatorService} from '../../services/itemValidator.service';
 import {StringEmptyService} from "../../services/stringEmpty.service";
 
-jest.mock('../../services/itemValidator.service', () => {
-    return class {
-        constructor() {
 
-        }
+jest.mock('../../services/itemValidator.service');
 
-        isContentValid(content: string): boolean {
-            return true;
-        }
-
-        isNameNotEmpty(name: string): boolean {
-            return true;
-        }
-
-        isCreatedDateValid(createdDate: Date): boolean {
-            return true;
-        }
-    }
-})
 
 describe('tests the validation for the creation of an item', () => {
 
 
     it('Should return true when all the validations are correct', () => {
-        let item: ItemModel = new ItemModel("jean", new Date,  "pomme");
+        ItemValidatorService.prototype.isCreatedDateValid = jest.fn().mockImplementationOnce(() => {
+            return true;
+        });
+        ItemValidatorService.prototype.isNameNotEmpty = jest.fn().mockImplementationOnce(() => {
+            return true;
+        });
+        ItemValidatorService.prototype.isContentValid = jest.fn().mockImplementationOnce(() => {
+            return true;
+        });
+        let item: ItemModel = new ItemModel("", new Date,  "");
         expect(item.isValid()).toBeTruthy();
+    });
+
+    it('Should return false when isNameNotEmpty is not valid', () => {
+        ItemValidatorService.prototype.isCreatedDateValid = jest.fn().mockImplementationOnce(() => {
+            return true;
+        });
+        ItemValidatorService.prototype.isNameNotEmpty = jest.fn().mockImplementationOnce(() => {
+            return false;
+        });
+        ItemValidatorService.prototype.isContentValid = jest.fn().mockImplementationOnce(() => {
+            return true;
+        });
+        let item: ItemModel = new ItemModel("", new Date,  "");
+        expect(item.isValid()).toBeFalsy();
+    });
+
+    it('Should return false when isContentValid is not valid', () => {
+        ItemValidatorService.prototype.isCreatedDateValid = jest.fn().mockImplementationOnce(() => {
+            return true;
+        });
+        ItemValidatorService.prototype.isNameNotEmpty = jest.fn().mockImplementationOnce(() => {
+            return true;
+        });
+        ItemValidatorService.prototype.isContentValid = jest.fn().mockImplementationOnce(() => {
+            return false;
+        });
+        let item: ItemModel = new ItemModel("", new Date,  "");
+        expect(item.isValid()).toBeFalsy();
+    });
+
+    it('Should return false when isCreatedDateValid is not valid', () => {
+        ItemValidatorService.prototype.isCreatedDateValid = jest.fn().mockImplementationOnce(() => {
+            return false;
+        });
+        ItemValidatorService.prototype.isNameNotEmpty = jest.fn().mockImplementationOnce(() => {
+            return true;
+        });
+        ItemValidatorService.prototype.isContentValid = jest.fn().mockImplementationOnce(() => {
+            return true;
+        });
+        let item: ItemModel = new ItemModel("", new Date,  "");
+        expect(item.isValid()).toBeFalsy();
     });
 });
