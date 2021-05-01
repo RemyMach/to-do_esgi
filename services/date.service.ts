d8baf14ddb79a691dc0316e32df6ee3a38985fe1
Quentin MOLERO <quentin.molero@estiam.com>
2021-05-01 13:33:29 +0200

    Fixed date check problem for item

--- services/date.service.ts
+++ services/date.service.ts
@@ -10,6 +10,6 @@ export class DateService {
     isItemCreationDateStamped(lastItem: ItemModel, newItem: ItemModel): boolean {
         const lastItemCreationDate = lastItem.createdAt;
         const newItemCreationDate = newItem.createdAt;
-        return (new Date(lastItemCreationDate.getTime() * 30 * 60 * 1000)).getTime() < newItemCreationDate.getTime();
+        return (new Date(lastItemCreationDate.getTime() + 30 * 60 * 1000)).getTime() < newItemCreationDate.getTime();
     }
 }
