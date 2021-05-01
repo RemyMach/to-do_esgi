272ba4ddb2267208cccad12027c5768bb22fbacc
Quentin MOLERO <quentin.molero@estiam.com>
2021-05-01 13:19:14 +0200

    Updated date service

--- services/birthdate.service.ts
+++
@@ -1,7 +0,0 @@
-export class BirthdateService {
-
-    isBirthdateValid(birthdate: Date): boolean {
-        birthdate.setUTCFullYear(birthdate.getUTCFullYear() + 13);
-        return birthdate.getTime() <= (new Date()).getTime();
-    }
-}
