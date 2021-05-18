0dde42d8f03d777482f4539b6cbe95d7f68a1001
Quentin MOLERO <quentin.molero@estiam.com>
2021-05-18 20:24:59 +0200

    Removed hardcoded date in toDoList test service

--- tests/services/toDoListValidator.test.ts
+++ tests/services/toDoListValidator.test.ts
@@ -8,13 +8,16 @@ describe("Tests to validate the toDoList service", () => {
     let dateService: DateService;
     let toDoListValidatorService: ToDoListValidatorService;
     let toDoList: ToDoListModel;
+    let itemCreationDate: Date;
     let newItem: ItemModel;
 
     beforeEach(() => {
         dateService = new DateService();
         toDoListValidatorService = new ToDoListValidatorService();
         toDoList = new ToDoListModel();
-        newItem = new ItemModel("Test", new Date(19, 1, 2000), "Me");
+        itemCreationDate = new Date();
+        itemCreationDate.setUTCFullYear(itemCreationDate.getFullYear() - 5);
+        newItem = new ItemModel("Test", itemCreationDate, "Me");
     });
 
     it("The item list should not be full if there is less than 10 items in it", () => {
@@ -37,7 +40,7 @@ describe("Tests to validate the toDoList service", () => {
     });
 
     it("Test getLastItem", () => {
-        const testItem = new ItemModel("Other test", new Date(19, 1, 2000), "You");
+        const testItem = new ItemModel("Other test", itemCreationDate, "You");
         toDoList.items = new Array(5).fill(newItem);
         toDoList.items.push(testItem);
         expect(toDoListValidatorService.getLastItem(toDoList.items)).toBe(testItem);
@@ -51,7 +54,7 @@ describe("Tests to validate the toDoList service", () => {
 
     it("The newItem name should be unique", () => {
         toDoList.items = new Array(5).fill(newItem);
-        const testItem = new ItemModel("Other test", new Date(19, 1, 2000), "You");
+        const testItem = new ItemModel("Other test", itemCreationDate, "You");
         expect(toDoListValidatorService.newItemNameIsUnique(toDoList.items, testItem)).toBeTruthy();
     });
 
@@ -71,17 +74,17 @@ describe("Tests to validate the toDoList service", () => {
 
     it("updateItemContent should return the same item list if the item name doesn't exist", () => {
         toDoList.items = new Array(5).fill(newItem);
-        const testItem = new ItemModel("Other test", new Date(19, 1, 2000), "You");
+        const testItem = new ItemModel("Other test", itemCreationDate, "You");
         expect(toDoListValidatorService.updateItemContent(testItem.name, "En fait non", toDoList.items)).toStrictEqual(toDoList.items);
     });
 
     it("updateItemContent should only modify testItem1 content", () => {
-        const testItem1 = new ItemModel("Other test", new Date(19, 1, 2000), "You");
-        const testItem2 = new ItemModel("test again", new Date(19, 1, 2000), "You again");
+        const testItem1 = new ItemModel("Other test", itemCreationDate, "You");
+        const testItem2 = new ItemModel("test again", itemCreationDate, "You again");
         toDoList.items = [testItem1, testItem2];
 
         toDoList.items = toDoListValidatorService.updateItemContent("You", "test", toDoList.items);
-        const newTestItem1 = new ItemModel("test", new Date(19, 1, 2000), "You");
+        const newTestItem1 = new ItemModel("test", itemCreationDate, "You");
 
         expect(toDoList.items[0]).toStrictEqual(newTestItem1);
         expect(toDoList.items[1]).toStrictEqual(testItem2);
@@ -89,13 +92,13 @@ describe("Tests to validate the toDoList service", () => {
 
     it("deleteItemByName should return the same item list if the item name doesn't exist", () => {
         toDoList.items = new Array(5).fill(newItem);
-        const testItem = new ItemModel("Other test", new Date(19, 1, 2000), "You");
+        const testItem = new ItemModel("Other test", itemCreationDate, "You");
         expect(toDoListValidatorService.deleteItemByName(testItem.name, toDoList.items)).toStrictEqual(toDoList.items);
     });
 
     it("updateItemContent should only delete testItem1", () => {
-        const testItem1 = new ItemModel("Other test", new Date(19, 1, 2000), "You");
-        const testItem2 = new ItemModel("test again", new Date(19, 1, 2000), "You again");
+        const testItem1 = new ItemModel("Other test", itemCreationDate, "You");
+        const testItem2 = new ItemModel("test again", itemCreationDate, "You again");
         toDoList.items = [testItem1, testItem2];
 
         expect(toDoListValidatorService.deleteItemByName(testItem1.name, toDoList.items)).toStrictEqual([testItem2]);
