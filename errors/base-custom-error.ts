9c60306bf5911b92db78a205ddb90db8ec7ee8c3
RemyMach <remygt@hotmail.fr>
2021-05-20 15:36:07 +0200

    add error system to project

---
+++ errors/base-custom-error.ts
@@ -0,0 +1,14 @@
+import { SerializedErrorOutput } from './types/serialized-error-output';
+
+export default abstract class BaseCustomError extends Error {
+    protected constructor(message?: string) {
+        super(message);
+
+        Object.setPrototypeOf(this, BaseCustomError.prototype);
+    }
+
+    abstract getStatusCode(): number;
+
+    abstract serializeErrorOutput(): SerializedErrorOutput;
+}
+
