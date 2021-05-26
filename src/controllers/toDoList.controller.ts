ea615ca96b3e7e2a4c63e7abd9f3f5de95bb5475
QuentinR <rouvilleq@gmail.com>
2021-05-26 19:12:18 +0200

    Add some methods to toDoList.controller.ts

--- src/controllers/toDoList.controller.ts
+++ src/controllers/toDoList.controller.ts
@@ -2,24 +2,65 @@ import {ModelCtor} from "sequelize";
 import {UserInstance} from "../models/user.model";
 import {SequelizeManager} from "../models";
 import {ToDoListInstance} from "../models/toDoList.model";
+import {ItemInstance} from "../models/item.model";
 
 export class ToDoListController
 {
     toDoList: ModelCtor<ToDoListInstance>;
     user: ModelCtor<UserInstance>;
+    item: ModelCtor<ItemInstance>;
 
     private static instance: ToDoListController;
 
     public static async getInstance(): Promise<ToDoListController> {
         if (ToDoListController.instance === undefined) {
-            const {toDoList, user} = await SequelizeManager.getInstance();
-            ToDoListController.instance = new ToDoListController(toDoList, user);
+            const {toDoList, user, item} = await SequelizeManager.getInstance();
+            ToDoListController.instance = new ToDoListController(toDoList, user, item);
         }
         return ToDoListController.instance;
     }
 
-    private constructor(toDoList: ModelCtor<ToDoListInstance>, user: ModelCtor<UserInstance>) {
+    private constructor(toDoList: ModelCtor<ToDoListInstance>, user: ModelCtor<UserInstance>, item: ModelCtor<ItemInstance>) {
         this.toDoList = toDoList;
         this.user = user;
+        this.item = item;
+    }
+
+    public async getToDoListById(id: number): Promise<ToDoListInstance | null>
+    {
+        return await this.toDoList.findOne({
+            attributes: ['id', 'name'],
+            where: {
+                id
+            }
+        });
+    }
+
+    public async getToDoListsByUserId(user_id: number): Promise<ToDoListInstance[] | null>
+    {
+        return await this.toDoList.findAll({
+            attributes: ['id', 'name'],
+            include: {
+                model: this.user,
+                required: true,
+                where: {
+                    user: user_id
+                }
+            }
+        });
+    }
+
+    public async getToDoListItemsWithToDoListId(id: number): Promise<ItemInstance[] | null>
+    {
+        return await this.item.findAll({
+            attributes: ['id', 'name', 'content'],
+            include: {
+                model: this.toDoList,
+                required: true,
+                where: {
+                    user: id
+                }
+            }
+        });
     }
 }
